7e26b320e2920d927b4b4bcdfed2bf9dbbcb905f
Ian Lewis <ianmlewis@gmail.com>
2023-09-17 04:51:46 +0000

    Add tag pages
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

---
+++ _plugins/tags.rb
@@ -0,0 +1,27 @@
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.to_set
+      tags.each do |tag|
+        site.pages << TagPage.new(site, site.source, "en", tag)
+        site.pages << TagPage.new(site, site.source, "jp", tag)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, category, tag)
+      @site = site
+      @base = base
+      @dir  = File.join(category, 'tag', tag)
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Tag: #{tag}"
+    end
+  end
+end
