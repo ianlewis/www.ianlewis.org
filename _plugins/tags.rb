c9306cd897f40d8cdd78a8210f375ac9043be132
Ian Lewis <ianlewis@google.com>
2024-07-03 18:46:18 +0900

    Paginate tag pages (#74)
    Fixes #49
    
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -1,3 +1,17 @@
+# Copyright 2023 Ian Lewis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 module Jekyll
   class TagPageGenerator < Generator
     safe true
@@ -20,9 +34,16 @@ module Jekyll
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['layout'] = 'tag'
       self.data['tag'] = tag
       self.data['category'] = category
       self.data['title'] = "Tag: #{tag}"
+      self.data['blog'] = category
+      self.data['pagination'] = {
+        'enabled' => true,
+        'category' => category,
+        'tag' => tag,
+      }
     end
   end
 end
