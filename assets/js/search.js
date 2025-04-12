4779c4bb4157e151ae509078e44b2848bb1502e0
Ian Lewis <ian@ianlewis.org>
2025-04-12 07:32:35 +0000

    feat: Update scss and js
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,47 +1,52 @@
 // Copyright (c) 2019 Samarjeet
 
 window.onload = function () {
-    var $searchbar = document.getElementById('searchbar');
-    var $searchResults = document.getElementById('search-results');
+  const $searchbar = document.getElementById("searchbar");
+  const $searchResults = document.getElementById("search-results");
 
-    if (!$searchbar || !$searchResults)
-        return;
+  if (!$searchbar || !$searchResults) return;
 
-    SimpleJekyllSearch({
-        searchInput: $searchbar,
-        resultsContainer: $searchResults,
-        json: '{{ "/search.json" | relative_url }}',
-        searchResultTemplate: '<a href="{url}" target="_blank">{title}</a>',
-        noResultsText: ''
-    });
+  // eslint-disable-next-line no-undef
+  SimpleJekyllSearch({
+    searchInput: $searchbar,
+    resultsContainer: $searchResults,
+    json: '{{ "/search.json" | relative_url }}',
+    // SimpleJekyllSearch doesn't use string template literals.
+    // eslint-disable-next-line github/unescaped-html-literal
+    searchResultTemplate: '<a href="{url}" target="_blank">{title}</a>',
+    noResultsText: "",
+  });
 
-    /* hack ios safari unfocus */
-    if (/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent))
-        document.body.firstElementChild.tabIndex = 1;
+  /* hack ios safari unfocus */
+  if (/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent))
+    document.body.firstElementChild.tabIndex = 1;
 
-    var $labelGroup = document.querySelector(".posts-labelgroup");
-    var $postLabel = document.getElementById("posts-label");
-    var labelWidth = $postLabel.scrollWidth;
+  const $labelGroup = document.querySelector(".posts-labelgroup");
+  const $postLabel = document.getElementById("posts-label");
+  const labelWidth = $postLabel.scrollWidth;
 
-    $postLabel.style.width = labelWidth + "px";
+  $postLabel.style.width = `${labelWidth}px`;
 
-    $labelGroup.addEventListener("click", function (e) {
-        $searchResults.style.display = null;
-        $postLabel.style.width = "0";
-        $labelGroup.setAttribute("class", "posts-labelgroup focus-within");
-        $searchbar.focus();
-        e.stopPropagation();
-    }, false);
+  $labelGroup.addEventListener(
+    "click",
+    function (e) {
+      $searchResults.style.display = null;
+      $postLabel.style.width = "0";
+      $labelGroup.setAttribute("class", "posts-labelgroup focus-within");
+      $searchbar.focus();
+      e.stopPropagation();
+    },
+    false,
+  );
 
-    $labelGroup.addEventListener("mouseleave", function () {
-        document.body.onclick = searchCollapse;
-    });
+  $labelGroup.addEventListener("mouseleave", function () {
+    document.body.onclick = searchCollapse;
+  });
 
-    var searchCollapse = function (e) {
-        $searchResults.style.display = "none";
-        $labelGroup.setAttribute("class", "posts-labelgroup");
-        $postLabel.style.width = labelWidth + "px";
-        document.body.onclick = null;
-    };
-}
-
+  function searchCollapse() {
+    $searchResults.style.display = "none";
+    $labelGroup.setAttribute("class", "posts-labelgroup");
+    $postLabel.style.width = `${labelWidth}px`;
+    document.body.onclick = null;
+  }
+};
